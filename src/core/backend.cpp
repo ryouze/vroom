ef9d9cbb7695ce244349c674ed473e8975f5d72e
ryouze <98982999+ryouze@users.noreply.github.com>
2025-05-01 01:04:57 +0200

    Remove config based window backend, use explicit variables, fix bugs.

--- src/core/backend.cpp
+++ src/core/backend.cpp
@@ -3,7 +3,6 @@
  */
 
 #include <algorithm>  // for std::clamp
-#include <optional>   // for std::optional
 
 #include <SFML/Graphics.hpp>
 #include <spdlog/spdlog.h>
@@ -16,60 +15,54 @@
 namespace core::backend {
 
 // TODO: Add spdlog everywhere
-Window::Window(const WindowConfig &config)
-    : config_(config),
-      fullscreen_(config.start_fullscreen)
+Window::Window()
+    : fullscreen_(default_start_fullscreen_),
+      vsync_enabled_(default_vsync_enabled_),
+      frame_limit_(default_frame_limit_),
+      windowed_resolution_(default_windowed_resolution_)
 {
     const sf::VideoMode initial_mode = this->fullscreen_
                                            ? sf::VideoMode::getDesktopMode()
-                                           : sf::VideoMode{config.windowed_resolution};
+                                           : sf::VideoMode{this->windowed_resolution_, default_bits_per_pixel_};
     const sf::State initial_state = this->fullscreen_
                                         ? sf::State::Fullscreen
                                         : sf::State::Windowed;
     this->create_window(initial_mode, initial_state);
 }
 
-void Window::set_fullscreen(const bool enable,
-                            const sf::VideoMode &video_mode)
+void Window::set_window(const bool enable,
+                        const sf::VideoMode &mode)
 {
-    // Not sure if this should be here!
-    // if (enable == this->fullscreen_) {
-    //     return;
-    // }
-    if (enable) {  // To fullscreen
-        this->config_.windowed_resolution = this->window_.getSize();
-        this->fullscreen_ = true;
-        this->recreate_window(video_mode, sf::State::Fullscreen);
+    // Only skip if disabling when already windowed
+    if (!enable && !this->fullscreen_) {
+        return;
+    }
+
+    this->fullscreen_ = enable;
+    if (this->fullscreen_) {  // To fullscreen
+        // Always recreate when enabling, even if already fullscreen
+        this->recreate_window(mode, sf::State::Fullscreen);
     }
     else {  // To windowed
-        this->fullscreen_ = false;
-        const sf::VideoMode windowed_mode{this->config_.windowed_resolution};
+        sf::VideoMode windowed_mode{this->windowed_resolution_, default_bits_per_pixel_};
         this->recreate_window(windowed_mode, sf::State::Windowed);
     }
 }
 
-// void Window::set_windowed_resolution(const sf::Vector2u &resolution)
-// {
-//     if (this->fullscreen_) {
-//         return;
-//     }
-//     this->config_.windowed_resolution = resolution;
-//     this->recreate_window(sf::VideoMode{resolution}, sf::State::Windowed);
-// }
-
-// TODO: Add checks and warnings if tried to enable both at the same time
 void Window::set_fps_limit(const unsigned fps_limit)
 {
-    this->config_.vsync = false;
-    this->config_.frame_limit = fps_limit;
+    // Disallow fps limit when vsync is on
+    this->vsync_enabled_ = false;
+    this->frame_limit_ = fps_limit;
     this->apply_sync_settings();
 }
 
 void Window::set_vsync(const bool enable)
 {
-    this->config_.vsync = enable;
-    if (enable) {
-        this->config_.frame_limit = 0;
+    // Disallow vsync when fps limit is set
+    this->vsync_enabled_ = enable;
+    if (this->vsync_enabled_) {
+        this->frame_limit_ = 0;
     }
     this->apply_sync_settings();
 }
@@ -87,31 +80,6 @@ void Window::run(const event_callback_type &on_event,
         constexpr float dt_max = 0.1f;
         const float dt = std::clamp(clock.restart().asSeconds(), dt_min, dt_max);
         on_update(dt);
-
-#ifndef NDEBUG  // Debug, remove later
-        const ImGuiViewport *vp = ImGui::GetMainViewport();
-        ImGui::SetNextWindowPos(ImVec2(vp->WorkPos.x + vp->WorkSize.x, vp->WorkPos.y), ImGuiCond_Always, ImVec2(1.0f, 0.0f));
-        ImGui::SetNextWindowSize(ImVec2(320.0f, 360.0f), ImGuiCond_Always);
-        if (ImGui::Begin("Window Debugging", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoBackground)) {
-            ImGui::TextUnformatted("Config:");
-            ImGui::BulletText(".minimum_size: %dx%d", this->config_.minimum_size.x, this->config_.minimum_size.y);
-            ImGui::BulletText(".windowed_resolution: %dx%d", this->config_.windowed_resolution.x, this->config_.windowed_resolution.y);
-            ImGui::BulletText(".title: %s", this->config_.title.c_str());
-            ImGui::BulletText(".frame_limit: %u", this->config_.frame_limit);
-            ImGui::BulletText(".vsync: %s", this->config_.vsync ? "true" : "false");
-            ImGui::BulletText(".start_fullscreen: %s", this->config_.start_fullscreen ? "true" : "false");
-            ImGui::BulletText(".anti_aliasing_level: %u", this->config_.anti_aliasing_level);
-            ImGui::TextUnformatted("Member Variables:");
-            ImGui::BulletText("this->fullscreen_: %s", this->fullscreen_ ? "true" : "false");
-            const auto window_settings = this->window_.getSettings();
-            const auto window_size_u = this->window_.getSize();
-            ImGui::TextUnformatted("SFML:");
-            ImGui::BulletText("Current Resolution: %dx%d", window_size_u.x, window_size_u.y);
-            ImGui::BulletText("Current Anti-Aliasing Level: %d", window_settings.antiAliasingLevel);
-            ImGui::BulletText("Current OpenGL Version: %d.%d", window_settings.majorVersion, window_settings.minorVersion);
-        }
-        ImGui::End();
-#endif
         on_render(this->window_);
     }
 }
@@ -119,32 +87,23 @@ void Window::run(const event_callback_type &on_event,
 void Window::create_window(const sf::VideoMode &mode,
                            const sf::State state)
 {
-    // Set anti-aliasing level to 8x
-    const sf::ContextSettings context_settings = {.antiAliasingLevel = this->config_.anti_aliasing_level};
-    this->window_.create(mode, this->config_.title, state, context_settings);
-    this->window_.setMinimumSize(std::optional<sf::Vector2u>{this->config_.minimum_size});
+    const sf::ContextSettings settings{.antiAliasingLevel = this->default_anti_aliasing_level_};
+    this->window_.create(mode, this->default_title_, state, settings);
+    this->window_.setMinimumSize(this->default_minimum_size_);
     this->apply_sync_settings();
 }
 
 void Window::recreate_window(const sf::VideoMode &mode,
                              const sf::State state)
 {
-    // TODO: Re-creating window seems to break anti-aliasing?
-    // And OpenGL gets weird too, as if it was random garbage value of int instead of actual value
     this->window_.close();
     this->create_window(mode, state);
 }
 
 void Window::apply_sync_settings()
 {
-    if (this->config_.vsync) {
-        this->window_.setVerticalSyncEnabled(true);
-        this->window_.setFramerateLimit(0);
-    }
-    else {
-        this->window_.setVerticalSyncEnabled(false);
-        this->window_.setFramerateLimit(this->config_.frame_limit);
-    }
+    this->window_.setVerticalSyncEnabled(this->vsync_enabled_);
+    this->window_.setFramerateLimit(this->vsync_enabled_ ? 0u : this->frame_limit_);
 }
 
 }  // namespace core::backend
--- src/core/backend.hpp
+++ src/core/backend.hpp
@@ -16,17 +16,11 @@
 
 namespace core::backend {
 
-// Static configuration
-struct WindowConfig {
-    sf::Vector2u minimum_size = {800, 600};
-    sf::Vector2u windowed_resolution = {1280, 720};
-    std::string title = std::format("{} ({})", generated::PROJECT_NAME, generated::PROJECT_VERSION);
-    unsigned frame_limit = 144;  // 0 == uncapped
-    bool vsync = false;
-    bool start_fullscreen = true;
-    unsigned anti_aliasing_level = 8;
-    // TODO: Allow user to change anti-aliasing level
-};
+// TODO: Use this in "set_window()"
+// enum class WindowState {
+//     Fullscreen,
+//     Windowed,
+// };
 
 class Window {
   public:
@@ -34,34 +28,31 @@ class Window {
     using update_callback_type = std::function<void(const float)>;
     using render_callback_type = std::function<void(sf::RenderWindow &)>;
 
-    explicit Window(const WindowConfig &config = WindowConfig());  // Use default config
+    // TODO: Set initial screen resolution, fps limit vsync, AA, etc. from config file on disk
+    explicit Window();
 
     // Default destructor
     ~Window() = default;
 
-    // TODO: Move this to .cpp once the API is stable
+    // Do not put these simple getters/setters in .cpp
     [[nodiscard]] bool is_fullscreen() const { return this->fullscreen_; }
-    [[nodiscard]] bool is_vsync_enabled() const { return this->config_.vsync; }
-    [[nodiscard]] const sf::ContextSettings &get_settings() const { return this->window_.getSettings(); }
+    [[nodiscard]] bool is_vsync_enabled() const { return this->vsync_enabled_; }
     [[nodiscard]] sf::RenderWindow &raw() { return this->window_; }
     [[nodiscard]] const sf::RenderWindow &raw() const { return this->window_; }
-    [[nodiscard]] sf::Vector2u get_size() const { return this->window_.getSize(); }
-
+    [[nodiscard]] sf::Vector2u get_resolution() const { return this->window_.getSize(); }
     void request_focus() { this->window_.requestFocus(); }
     void set_view(const sf::View &view) { this->window_.setView(view); }
     void clear(const sf::Color &color) { this->window_.clear(color); }
-    template <typename D>
-    void draw(const D &drawable) { this->window_.draw(drawable); }
+    template <typename Drawable>
+    void draw(const Drawable &drawable) { this->window_.draw(drawable); }
     void display() { this->window_.display(); }
     void close() { this->window_.close(); }
 
-    void set_fullscreen(const bool enable,
-                        const sf::VideoMode &video_mode = sf::VideoMode::getDesktopMode());
-
-    // void set_windowed_resolution(const sf::Vector2u &resolution);
+    void set_window(const bool enable,  // <-- Replace this with enum
+                    const sf::VideoMode &mode = sf::VideoMode::getDesktopMode());
 
     // FPS or V-sync (exclusive)
-    void set_fps_limit(const unsigned fps_limit);  // 0 == uncapped
+    void set_fps_limit(const unsigned fps_limit);
     void set_vsync(const bool enable);
 
     // Main-loop helper
@@ -82,8 +73,22 @@ class Window {
                          const sf::State state);
     void apply_sync_settings();
 
-    WindowConfig config_;
-    bool fullscreen_;
+    // Fixed defaults
+    static constexpr sf::Vector2u default_windowed_resolution_ = {1280, 720};
+    static constexpr sf::Vector2u default_minimum_size_ = {800, 600};
+    static constexpr unsigned default_bits_per_pixel_ = 32;
+    static inline const std::string default_title_ = std::format("{} ({})", generated::PROJECT_NAME, generated::PROJECT_VERSION);
+    static constexpr unsigned default_frame_limit_ = 144;
+    static constexpr bool default_vsync_enabled_ = false;
+    static constexpr bool default_start_fullscreen_ = true;
+    static constexpr unsigned default_anti_aliasing_level_ = 8;
+
+    // Runtime state
+    bool fullscreen_ = default_start_fullscreen_;
+    bool vsync_enabled_ = default_vsync_enabled_;
+    unsigned frame_limit_ = default_frame_limit_;
+    sf::Vector2u windowed_resolution_ = default_windowed_resolution_;
+
     sf::RenderWindow window_;
 };
 
