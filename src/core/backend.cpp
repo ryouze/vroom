3561eb89390dda1c56c02db2daa2f0b2d71c5b25
ryouze <98982999+ryouze@users.noreply.github.com>
2025-06-01 23:34:36 +0200

    Remove useless sf::Vector calls.

--- src/core/backend.cpp
+++ src/core/backend.cpp
@@ -126,7 +126,7 @@ void Window::apply_sync_settings()
 
 sf::Vector2f to_vector2f(const sf::Vector2u &vector)
 {
-    return sf::Vector2f(static_cast<float>(vector.x), static_cast<float>(vector.y));
+    return {static_cast<float>(vector.x), static_cast<float>(vector.y)};
 }
 
 }  // namespace core::backend
