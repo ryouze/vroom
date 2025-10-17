cfbb46b00b983c8c1c33871fb531cbe52ba89729
ryouze <98982999+ryouze@users.noreply.github.com>
2025-10-17 19:26:11 +0200

    Use explicit ".data" and ".size".

--- src/assets/builder.cpp
+++ src/assets/builder.cpp
@@ -38,20 +38,20 @@ assets::textures::TextureManager build_texture_manager()
     assets::textures::TextureManager texture_manager;
 
     // Road textures
-    texture_manager.load("top_left", {road_sand89::data, road_sand89::size});
-    texture_manager.load("top_right", {road_sand01::data, road_sand01::size});
-    texture_manager.load("bottom_right", {road_sand37::data, road_sand37::size});
-    texture_manager.load("bottom_left", {road_sand35::data, road_sand35::size});
-    texture_manager.load("vertical", {road_sand87::data, road_sand87::size});
-    texture_manager.load("horizontal", {road_sand88::data, road_sand88::size});
-    texture_manager.load("horizontal_finish", {road_sand39::data, road_sand39::size});
+    texture_manager.load("top_left", {.data = road_sand89::data, .size = road_sand89::size});
+    texture_manager.load("top_right", {.data = road_sand01::data, .size = road_sand01::size});
+    texture_manager.load("bottom_right", {.data = road_sand37::data, .size = road_sand37::size});
+    texture_manager.load("bottom_left", {.data = road_sand35::data, .size = road_sand35::size});
+    texture_manager.load("vertical", {.data = road_sand87::data, .size = road_sand87::size});
+    texture_manager.load("horizontal", {.data = road_sand88::data, .size = road_sand88::size});
+    texture_manager.load("horizontal_finish", {.data = road_sand39::data, .size = road_sand39::size});
 
     // Car textures
-    texture_manager.load("car_black", {car_black_1::data, car_black_1::size});
-    texture_manager.load("car_blue", {car_blue_1::data, car_blue_1::size});
-    texture_manager.load("car_green", {car_green_1::data, car_green_1::size});
-    texture_manager.load("car_red", {car_red_1::data, car_red_1::size});
-    texture_manager.load("car_yellow", {car_yellow_1::data, car_yellow_1::size});
+    texture_manager.load("car_black", {.data = car_black_1::data, .size = car_black_1::size});
+    texture_manager.load("car_blue", {.data = car_blue_1::data, .size = car_blue_1::size});
+    texture_manager.load("car_green", {.data = car_green_1::data, .size = car_green_1::size});
+    texture_manager.load("car_red", {.data = car_red_1::data, .size = car_red_1::size});
+    texture_manager.load("car_yellow", {.data = car_yellow_1::data, .size = car_yellow_1::size});
 
     return texture_manager;
 }
@@ -61,13 +61,13 @@ assets::sounds::SoundManager build_sound_manager()
     assets::sounds::SoundManager sound_manager;
 
     // Car sounds
-    sound_manager.load("engine", {engine::data, engine::size});
-    sound_manager.load("tires", {tires::data, tires::size});
-    sound_manager.load("hit", {hit::data, hit::size});
+    sound_manager.load("engine", {.data = engine::data, .size = engine::size});
+    sound_manager.load("tires", {.data = tires::data, .size = tires::size});
+    sound_manager.load("hit", {.data = hit::data, .size = hit::size});
 
     // UI sounds
-    sound_manager.load("ok", {ok::data, ok::size});
-    sound_manager.load("other", {other::data, other::size});
+    sound_manager.load("ok", {.data = ok::data, .size = ok::size});
+    sound_manager.load("other", {.data = other::data, .size = other::size});
 
     return sound_manager;
 }
